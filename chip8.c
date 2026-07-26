dd409b75c3aacfd519ab650d7e72f77ab583934a
Joachim <joachimsh91@gmail.com>
2026-07-26 22:59:57 +0200

    Implemented more opcodes

--- chip8.c
+++ chip8.c
@@ -32,21 +32,59 @@ void chip8_emulateCycle(Chip8* system) {
 
     // Decode opcode
     switch(system->opcode & 0xF000)
-    {    
-        // Some opcode
-    
-        case 0xA000: // ANNN: Sets I to the address NNN
+    {   
+        case 0x1000: // 1NNN: Jumps to address NNN
+            system->pc = system->opcode & 0x0FFF;
+            break;
 
-        // Execute opcode
-        system->I = system->opcode & 0x0FFF;
-        system->pc += 2;
-        break;
-    
-        // More opcodes //
+        case 0x2000: // 2NNN: Calls subroutine at NNN
+
+            // Stores the return address for the next instruction on the stack where the stack pointer points
+            system->stack[system->sp] = system->pc + 2;
+
+            // Increases the stack pointer so it's ready for the next function call
+            system->sp++;
+
+            // Jump to the new address NNN
+            system->pc = system->opcode & 0x0FFF;
+            break;
+
+        case 0x3000: { //Skips the next instruction if VX equals NN (usually the next instruction is a jump to skip a code block)
+            uint8_t x = (system->opcode & 0x0F00) >> 8;
+            uint8_t nn = system->opcode & 0x00FF;
+            if (system->V[x] == nn){
+                system->pc +=2;
+            }
+            system->pc +=2;
+            break;
+    }
+        case 0x4000: { //Skips the next instruction if VX does not equal NN (usually the next instruction is a jump to skip a code block)
+            uint8_t x = (system->opcode & 0x0F00) >> 8;
+            uint8_t nn = system->opcode & 0x00FF;
+            if (system->V[x] != nn){
+                system->pc +=2;
+            }
+            system->pc +=2;
+            break;
+    }
+        case 0x5000: { //Skips the next instruction if VX equals VY (usually the next instruction is a jump to skip a code block)
+            uint8_t x = (system->opcode & 0x0F00) >> 8;
+            uint8_t y = (system->opcode & 0x00F0) >> 4;
+            if (system->V[x] == system->V[y]){
+                system->pc +=2;
+            }
+            system->pc +=2;
+            break;
+        }
+
+        case 0xA000: // ANNN: Sets I to the address NNN
+            system->I = system->opcode & 0x0FFF;
+            system->pc += 2;
+            break;
 
         case 0xB000: // BNNN: Jumps to the address NNN plus V0
-        system->pc = (system->opcode & 0x0FFF) + system->V[0];
-        break;
+            system->pc = (system->opcode & 0x0FFF) + system->V[0];
+            break;
     
         default:
         printf("Unknown opcode: 0x%X\n", system->opcode);
